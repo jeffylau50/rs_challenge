1c4a77a48ea3710257623f04e33e8b43619a044e
Jeffy Lau <100321067+jeffy2560@users.noreply.github.com>
2022-06-23 14:50:04 -0700

    Setting up basic team api

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { Button } from "@mui/material";
 import rsLogo from "./logo-with-name.png";
 import "./App.css";
@@ -7,13 +7,29 @@ import axios from 'axios';
 const bBallapi = "https://www.balldontlie.io/api/v1/teams"
 
 function App() {
+  
+  const [teams, setTeams] = useState([])
+  const [showContent, setShow] = useState(false)
+  useEffect(()=>{
+    const fetchData = async () => {
+    let res = await axios.get(bBallapi);
+    setTeams(res.data.data)
+    console.log(teams)}
+    fetchData();
+  }, [showContent])
+
+  const handleToggle = () => {
+    setShow(!(showContent))
+    console.log('dfdf'+teams)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={rsLogo} className="App-logo" alt="logo" />
       </header>
       <main>
-       
+      <button onClick={handleToggle} className="btn btn-warning">Click Me to show All NBA teams</button>
         
       </main>
     </div>
